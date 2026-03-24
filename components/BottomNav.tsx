6be7c7f841e80fcce13df1348482c3b081346fb9
Odiegodev1 <odiegodev10@gmail.com>
2026-03-24 19:49:11 -0300

    navbottom

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -1,30 +1,36 @@
 "use client";
 
-import { Home, Search, Heart, User } from "lucide-react";
+import { Home, Search, Heart, User, Ticket } from "lucide-react";
+import Link from "next/link";
 
 export function BottomNav() {
   return (
     <div className="fixed bottom-0 left-0 w-full h-16 bg-white border-t flex items-center justify-around z-50">
       
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
+      <Link href="/" className="flex flex-col items-center text-gray-600 hover:text-blue-500">
         <Home size={22} />
         <span className="text-xs">Home</span>
-      </button>
+      </Link>
+       <Link href="/eventos" className="flex flex-col items-center text-gray-600 hover:text-blue-500">
+        <Ticket size={22} />
+        <span className="text-xs">Eventos</span>
+      </Link>
 
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
+
+      <Link href="/search" className="flex flex-col items-center text-gray-600 hover:text-blue-500">
         <Search size={22} />
         <span className="text-xs">Buscar</span>
-      </button>
+      </Link>
 
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
+      <Link href="/favorites" className="flex flex-col items-center text-gray-600 hover:text-blue-500">
         <Heart size={22} />
         <span className="text-xs">Favoritos</span>
-      </button>
+      </Link>
 
-      <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
+      <Link href="/profile" className="flex flex-col items-center text-gray-600 hover:text-blue-500">
         <User size={22} />
         <span className="text-xs">Perfil</span>
-      </button>
+      </Link>
 
     </div>
   );
