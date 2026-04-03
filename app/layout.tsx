050ec908dd48ef09245a16dbe167114bc6644618
Odiegodev1 <odiegodev10@gmail.com>
2026-04-03 09:13:36 -0300

    loginv8

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">{children}
          <BottomNav />
          
-         <InstallButton />
+   
       </body>
      
     </html>
--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,3 @@
 // middleware.ts
 export { auth as middleware } from "@/lib/auth";
 
-export const config = {
-  matcher: ["/profile"],
-};
