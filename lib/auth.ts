e85e7a03003f536b29eab0c0858a913f2b27a7c7
Odiegodev1 <odiegodev10@gmail.com>
2026-04-03 10:00:56 -0300

    resolve conflitov1

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,14 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "./prisma";
-
+import { prisma } from "./prisma";
 
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.NEXTAUTH_SECRET!,
   trustHost: true,
   debug: true,
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma as any),
   providers: [
     Github({
       clientId: process.env.GITHUB_ID!,
